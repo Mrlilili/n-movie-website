41d4e897b0d17fd63f2cbf589bd10b4969a21d21
Aaron <15360085185>
2016-05-15 00:42:21 +0800

    新增修改，查看功能，调整路由逻辑

--- app.js
+++ app.js
@@ -5,81 +5,132 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var path = require('path');
+var mongoose = require('mongoose');
+var _ = require('underscore');
+var Movie = require('./models/movie')
 var port = process.env.PORT || 3000;
 var app = express();
+mongoose.connect('mongodb://localhost/imooc');
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
-//app.use(bodyParser());
+app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'bower_components')));
 app.listen(port);
 console.log('started on  port' + port);
-
 // index page
 app.get('/', function (req, res) {
-    res.render('index', {
-        title: '首页',
-        movies: [{
-            title: '机器战警',
-            _id: 1,
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }, {
-            title: '机器战警',
-            _id: 2,
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }, {
-            title: '机器战警',
-            _id: 3,
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }, {
-            title: '机器战警',
-            _id: 4,
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }, {
-            title: '机器战警',
-            _id: 5,
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }, {
-            title: '机器战警',
-            _id: 6,
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }, {
-            title: '机器战警',
-            _id: 7,
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }, {
-            title: '机器战警',
-            _id: 8,
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
-        }]
-    })
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
+        }
+        res.render('index', {
+            title: '首页',
+            movies: movies
+        })
+    });
+
 })
 // detail page
-app.get('/move/:id', function (req, res) {
-    res.render('detail', {
-        title: '详情',
-        movies: {
-            title: '机械战警',
-            doctor: '何塞·帕迪里亚',
-            country: '美国',
-            language: '英语',
-            year: '2014',
-            flash: 'http://player.youku.com/player.php/sid/XMTM3MjE3MzQ1Ng==/v.swf',
-            summary: '2028年，专事军火开发的机器人公司Omni Corp.生产了大量装备精良的机械战警，他们被投入到惩治犯罪等行动中，取得显著的效果。罪犯横行的底特律市，嫉恶如仇、正义感十足的警察亚历克斯·墨菲（乔尔·金纳曼 饰）遭到仇家暗算，身体受到毁灭性破坏。借助于Omni公司天才博士丹尼特·诺顿（加里·奥德曼 饰）最前沿的技术，墨菲以机械战警的形态复活。数轮严格的测试表明，墨菲足以承担起维护社会治安的重任，他的口碑在民众中直线飙升，而墨菲的妻子克拉拉（艾比·考尼什 饰）和儿子大卫却再难从他身上感觉亲人的温暖。 　　感知到妻儿的痛苦，墨菲决心向策划杀害自己的犯罪头子展开反击……',
-            poster: 'http://g3.ykimg.com/05160000530EEB63675839160D0B79D5'
+app.get('/movie/:id', function (req, res) {
+    var id = req.params.id;
+    if (id) {
+        Movie.findById(id, function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
 
-        }
-    })
+            res.render('detail', {
+                title: '详情',
+                movies: movie
+            })
+        })
+    }
+
 })
 // admin page
 app.get('/admin/movie', function (req, res) {
     res.render('admin', {
-        title: '管理'
+        title: '管理',
+        movie: {
+            _id: '',
+            title: '',
+            doctor: '',
+            country: '',
+            language: '',
+            poster: '',
+            flash: '',
+            year: '',
+            summary: ''
+        }
     })
 })
+// admin update movie
+app.get('/admin/update/:id', function (req, res) {
+    var id = req.params.id;
+    if (id) {
+        Movie.findById(id, function (err, movie) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('admin', {
+                    title: '更新',
+                    movie: movie[0]
+                })
+            }
+        })
+    }
+})
+// admin post movie
+app.post('/admin/movie/new', function (req, res) {
+    var id = req.body._id
+    var movieObj = req.body.movie
+    var _movie;
 
+    if (id != undefined) {
+        console.log(1, id);
+        Movie.findById(id, function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+            console.log(movie);
+            _movie = _.extend(movie[0], movieObj);
+            _movie.save(function (err, movie) {
+                if (err) {
+                    console.log(err);
+                }
+                res.redirect('/movie/' + _movie.id)
+            })
+        })
+    } else {
+        console.log(2, id);
+        _movie = new Movie({
+            doctor: movieObj.doctor,
+            title: movieObj.title,
+            country: movieObj.country,
+            language: movieObj.language,
+            year: movieObj.year,
+            poster: movieObj.poster,
+            summary: movieObj.summary,
+            flash: movieObj.flash
+        });
+        _movie.save(function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+            res.redirect('/movie/' + _movie.id)
+        });
+    }
+});
 // list page
 app.get('/admin/list', function (req, res) {
-    res.render('list', {
-        title: '列表'
+    Movie.fetch(function (err, movies) {
+        if (err) {
+            console.log(err);
+        }
+        res.render('list', {
+            title: '列表页',
+            movies: movies
+        })
     })
+
 })
