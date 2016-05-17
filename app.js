6e8ed7b381c13bf630e0abc0955152a0ef089182
Aaron <15360085185>
2016-05-17 23:06:52 +0800

    1.0.0

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ mongoose.connect('mongodb://localhost/imooc');
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
 app.use(bodyParser());
-app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(port);
+app.locals.moment = require('moment')
 console.log('started on  port' + port);
 // index page
 app.get('/', function (req, res) {
@@ -86,8 +87,8 @@ app.post('/admin/movie/new', function (req, res) {
     var movieObj = req.body.movie
     var _movie;
 
-    if (id != undefined) {
-        console.log(1, id);
+    if (id != '') {
+
         Movie.findById(id, function (err, movie) {
             if (err) {
                 console.log(err);
@@ -102,7 +103,7 @@ app.post('/admin/movie/new', function (req, res) {
             })
         })
     } else {
-        console.log(2, id);
+
         _movie = new Movie({
             doctor: movieObj.doctor,
             title: movieObj.title,
@@ -133,4 +134,20 @@ app.get('/admin/list', function (req, res) {
         })
     })
 
+})
+
+//list delete movie
+
+app.delete('/admin/list', function (req,res) {
+    var id = req.query.id;
+    if (id) {
+        Movie.remove({_id: id}, function (err, movie) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.json({success: 1});
+            }
+        })
+    }
 })
